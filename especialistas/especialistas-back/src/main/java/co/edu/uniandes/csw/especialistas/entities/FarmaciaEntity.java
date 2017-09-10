569714fbd1c136d8732c49a01ff0854c3fd5e1af
rc.tejon <rc.tejon@ISIS2603S2-0048.sis.virtual.uniandes.edu.co>
2017-09-09 20:43:18 -0500

    Entidad Farmacia y medicamento

---
+++ especialistas/especialistas-back/src/main/java/co/edu/uniandes/csw/especialistas/entities/FarmaciaEntity.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.especialistas.entities;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author rc.tejon
+ */
+@Entity
+public class FarmaciaEntity extends BaseEntity{
+    
+    
+}
