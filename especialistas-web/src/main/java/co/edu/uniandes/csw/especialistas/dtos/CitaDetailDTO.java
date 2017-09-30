a2560b23ecec6795d6aa78bd4b163c813af618de
jr.restom10 <jr.restom10@ISIS2603S1-0025.sis.virtual.uniandes.edu.co>
2017-09-29 20:29:36 -0500

    details de cita y orden

--- especialistas-web/src/main/java/co/edu/uniandes/csw/especialistas/dtos/CitaDetailDTO.java
+++ especialistas-web/src/main/java/co/edu/uniandes/csw/especialistas/dtos/CitaDetailDTO.java
@@ -5,11 +5,90 @@
  */
 package co.edu.uniandes.csw.especialistas.dtos;
 
+import co.edu.uniandes.csw.especialistas.entities.CitaEntity;
+import co.edu.uniandes.csw.especialistas.entities.HoraEntity;
+import co.edu.uniandes.csw.especialistas.entities.OrdenMedicaEntity;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
- * @author jl.patarroyo
+ * @author jr.restom10
  */
 public class CitaDetailDTO extends CitaDTO{
+    
+     private HoraDTO hora;
+     private UsuarioDTO usuario;
+     private List<OrdenMedicaDTO> ordenes;
+     
+     
+     public CitaDetailDTO() {
+    }
+
+    public CitaDetailDTO(CitaEntity entity) {
+        super(entity);
+        
+        if(entity.getOrdenesMedicas()!=null)
+        {
+            ordenes = new ArrayList();
+        
+        for(OrdenMedicaEntity orden:entity.getOrdenesMedicas())
+        {
+            ordenes.add(new OrdenMedicaDTO(orden));
+        }
+        
+        }
+        
+        this.hora = new HoraDTO(entity.getHora());
+      
+        this.usuario = new UsuarioDTO(entity.getUsuario());
+    }
+
+    public HoraDTO getHora() {
+        return hora;
+    }
+    public List<OrdenMedicaDTO> getOrdenesMedicas() {
+        return ordenes;
+    }
+    public UsuarioDTO getUsuario() {
+        return usuario;
+    }
+
+    public void setHora(HoraDTO hora) {
+        this.hora = hora;
+    }
+     public void setOrdenes(List<OrdenMedicaDTO> orden) {
+        this.ordenes = orden;
+    }
+      public void setUsuario(UsuarioDTO usuario) {
+        this.usuario= usuario;
+    }
+
+    @Override
+    public CitaEntity toEntity() {
+        CitaEntity entity = super.toEntity();
+        entity.setHora(hora.toEntity());
+        entity.setUsuario(usuario.toEntity());
+        
+        if(this.getOrdenesMedicas()!=null)
+        {
+        List<OrdenMedicaEntity> o= new ArrayList();
+        for(OrdenMedicaDTO orden:this.getOrdenesMedicas() )
+        {
+            o.add(orden.toEntity());
+        }
+        
+        
+        entity.setOrdenMedica(o);
+        }
+        return entity;
+    }
+
+    
+    
+    
+    
+    
     
     
     
