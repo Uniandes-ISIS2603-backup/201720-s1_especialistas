cae1d0666385d3f12657b6e645339e7980d8287b
rc.tejon <rc.tejon@ISIS2603S2-0048.sis.virtual.uniandes.edu.co>
2017-10-23 15:08:39 -0500

    kgaf

--- especialistas-web/src/main/webapp/src/modules/farmacias/farmacias.ctrl.js
+++ especialistas-web/src/main/webapp/src/modules/farmacias/farmacias.ctrl.js
@@ -1,7 +1,7 @@
 (function (ng) {
     var mod = ng.module("farmaciaModule");
     mod.constant("farmaciasContext", "api/farmacias");
-    mod.controller('farmaciasCtrl', ['$scope', '$http', 'farmaciasContext',
+    mod.controller('farmaciaCtrl', ['$scope', '$http', 'farmaciasContext',
         function ($scope, $http,farmaciasContext) {
             $http.get(farmaciasContext).then(function (response) {
                 $scope.farmacia = response.data;
