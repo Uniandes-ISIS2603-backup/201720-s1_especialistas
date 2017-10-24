da5a03e65d90354d308ec73bbd6217d0caa66adb
DIANA MARCELA GUTIERREZ CORDOBA <dm.gutierrez11@sis.virtual.uniandes.edu.co>
2017-10-23 21:16:49 -0500

    arreglo back

--- especialistas-web/src/main/java/co/edu/uniandes/csw/especialistas/dtos/ExamenDTO.java
+++ especialistas-web/src/main/java/co/edu/uniandes/csw/especialistas/dtos/ExamenDTO.java
@@ -6,6 +6,9 @@
 package co.edu.uniandes.csw.especialistas.dtos;
 
 import co.edu.uniandes.csw.especialistas.entities.ExamenEntity;
+import co.edu.uniandes.csw.especialistas.entities.LaboratorioEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -17,6 +20,7 @@ public class ExamenDTO {
     private String nombre;
     private Double precio;
     private String recomendacion;
+    private List<LaboratorioDetailDTO> labs;
 
     public ExamenDTO() {
 
@@ -32,6 +36,10 @@ public class ExamenDTO {
         this.nombre = examen.getnombre();
         this.precio = examen.getPrecio();
         this.recomendacion = examen.getRecomedacion();
+        if(examen.getLaboratorios() == null){
+            examen.setLaboratorios(new ArrayList<LaboratorioEntity>());
+        }
+        this.labs = labToDTO(examen.getLaboratorios());
     }
 
     //getters y setters
@@ -81,4 +89,10 @@ public class ExamenDTO {
         return examen;
     }
 
+    private List<LaboratorioDetailDTO> labToDTO(List<LaboratorioEntity> lista){
+        List<LaboratorioDetailDTO> labs = new ArrayList<LaboratorioDetailDTO>();
+        for( LaboratorioEntity a : lista)
+            labs.add(new LaboratorioDetailDTO(a));
+        return labs;
+    }
 }
