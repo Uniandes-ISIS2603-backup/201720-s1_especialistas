57b853a5b071b2acd8172bb6e838fd0729623203
jl.patarroyo <jl.patarroyo@ISIS2603S1-0036.sis.virtual.uniandes.edu.co>
2017-11-02 10:12:17 -0500

    Arreglo hospitales.ctrl.js

--- especialistas-web/src/main/webapp/src/modules/hospitales/hospitales.ctrl.js
+++ especialistas-web/src/main/webapp/src/modules/hospitales/hospitales.ctrl.js
@@ -1,7 +1,7 @@
 (function (ng) {
     var mod = ng.module("hospitalesModule");
     mod.constant("hospitalesContext", "api/hospitales");
-    mod.controller('hospitalCtrl', ['$scope', '$http', 'hospitales  Context',
+    mod.controller('hospitalCtrl', ['$scope', '$http', 'hospitalesContext',
         function ($scope, $http, hospitalesContext) {
             $http.get(hospitalesContext).then(function (response) {
                 $scope.hospitalesRecords = response.data;
