4a02b89c8a24e7a90ea8b79cfb7ec777da3e1350
Juan Sebastian Diaz <js.diaz@uniandes.edu.co>
2017-11-03 16:36:24 -0500

    detail

--- especialistas-web/src/main/webapp/src/modules/medicos/medicos.ctrl.js
+++ especialistas-web/src/main/webapp/src/modules/medicos/medicos.ctrl.js
@@ -1,11 +1,18 @@
 (function (ng) {
     var mod = ng.module("medicoModule");
     mod.constant("medicosContext", "api/medicos");
-    mod.controller('medicoCtrl', ['$scope', '$http', 'medicosContext',
-        function ($scope, $http, medicosContext) {
+    mod.controller('medicoCtrl', ['$scope', '$state', '$http', 'medicosContext',
+        function ($scope, $state, $http, medicosContext) {
             $http.get(medicosContext).then(function (response) {
                 $scope.medicosRecords = response.data;
             });
+            
+            if($state.params.medicoId !== undefined){
+                $http.get(medicosContext + '/' + $state.params.medicoId).then(function (response) {
+                    $scope.medico = response.data;
+                    $scope.agenda = response.data.agenda;
+                });
+            }
         }
     ]);
 }
