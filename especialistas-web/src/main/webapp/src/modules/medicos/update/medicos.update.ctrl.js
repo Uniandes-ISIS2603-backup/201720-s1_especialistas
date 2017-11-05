d79110ed4ee43f274c7b2d49f5d16361423f828d
Juan Sebastian Diaz <js.diaz@uniandes.edu.co>
2017-11-04 20:35:30 -0500

    update y delete

---
+++ especialistas-web/src/main/webapp/src/modules/medicos/update/medicos.update.ctrl.js
@@ -0,0 +1,33 @@
+(function (ng) {
+    var mod = ng.module("medicoModule");
+    mod.constant("medicosContext", "api/authors");
+    mod.controller('medicoUpdateCtrl', ['$scope', '$http', 'medicosContext', '$state', '$rootScope',
+        function ($scope, $http, medicosContext, $state, $rootScope) {
+            $rootScope.edit = true;
+            $scope.medicoId = $state.params.medicoId;
+            
+            $http.get("api/especializaciones").then(function (response) {
+                $scope.especializaciones = response.data;
+            });
+
+
+            $http.get(medicosContext + '/' + $scope.medicoId).then(function (response) {
+                var medico = response.data;
+                $scope.nombreMedico = medico.nombre;
+                $scope.especializacionMedico = medico.especializacion;
+            });
+
+            $scope.createMedico = function () {
+            $http.put(medicosContext + "/" + $scope.medicoId, {
+                nombre: $scope.nombreMedico,
+                especializacion: $scope.especializacionMedico,
+                agenda : []
+            }).then(function (response) {
+                $state.go('medicoDetail', {medicoId: response.data.id}, {reload: true});
+            });
+            };
+        }
+    ]);
+}
+)(angular);
+
