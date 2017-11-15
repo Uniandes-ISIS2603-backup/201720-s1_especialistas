57cda258cf79b05dc108848f8ecf2b19297d8f2c
jl.patarroyo <jl.patarroyo@ISIS2603S1-0036.sis.virtual.uniandes.edu.co>
2017-11-14 23:06:35 -0500

    DELETE hospitales

--- especialistas-web/src/main/java/co/edu/uniandes/csw/especialistas/resources/HospitalResource.java
+++ especialistas-web/src/main/java/co/edu/uniandes/csw/especialistas/resources/HospitalResource.java
@@ -29,6 +29,7 @@ import javax.ws.rs.WebApplicationException;
 
 /**
  * Clase que modela el recurso de hospitales
+ *
  * @author jl.patarroyo
  */
 @Path("hospitales")
@@ -36,70 +37,89 @@ import javax.ws.rs.WebApplicationException;
 @Consumes("application/json")
 @Stateless
 public class HospitalResource {
-    
+
     /**
      * Injección de la lógica de hospitales
      */
     @Inject
     HospitalLogic logic;
-    
+
     /**
      * Método encargado de obtener todos los hospitales
+     *
      * @return lista de hospitales
      */
     @GET
-    public List<HospitalDTO> getHospitales(){
-        try{
+    public List<HospitalDTO> getHospitales() {
+        try {
             List<HospitalEntity> listaEntidades = logic.getHospitales();
             List<HospitalDTO> listaHospitales = new ArrayList<>();
-            for(HospitalEntity entity: listaEntidades){
+            for (HospitalEntity entity : listaEntidades) {
                 HospitalDTO dto = new HospitalDTO(entity);
                 listaHospitales.add(dto);
             }
             return listaHospitales;
-        }catch(BusinessLogicException e){
+        } catch (BusinessLogicException e) {
             throw new WebApplicationException(e.getMessage(), 404);
         }
     }
-    
+
     /**
      * Método encargado de obtener un hospital por su id
+     *
      * @param id id del hospital
      * @return DetailDTO del hospital
      */
     @GET
     @Path("/{id: \\d+}")
-    public HospitalDetailDTO getHospital(@PathParam("id") Long id){
-        try{
+    public HospitalDetailDTO getHospital(@PathParam("id") Long id) {
+        try {
             HospitalEntity entidad = logic.getHospital(id);
             HospitalDetailDTO hospital = new HospitalDetailDTO(entidad);
             return hospital;
-        }catch(BusinessLogicException e){
+        } catch (BusinessLogicException e) {
             throw new WebApplicationException(e.getMessage(), 404);
         }
     }
-    
+
     /**
      * Método encargado de crear un hospital
+     *
      * @param hospital DTO del hospital
      * @return DTO del hospital creado
      */
     @POST
-    public HospitalDTO createHospital(HospitalDTO hospital){
+    public HospitalDTO createHospital(HospitalDTO hospital) {
         HospitalEntity entidad = hospital.toEntity();
-        try{
+        try {
             logic.createHospital(entidad);
             return hospital;
-        }catch(BusinessLogicException e){
+        } catch (BusinessLogicException e) {
             throw new WebApplicationException(e.getMessage(), 500);
         }
     }
-    
+
+    /**
+     * Método encargado de actualizar la información de un hospital
+     *
+     * @param hospital DetailDTO con la nueva información del hospital
+     * @return DetailDTO con la información actualizada
+     */
     @PUT
-    public HospitalDetailDTO updateHospital(HospitalDetailDTO hospital){
+    public HospitalDetailDTO updateHospital(HospitalDetailDTO hospital) {
         HospitalEntity entidad = hospital.toEntity();
         logic.updateHospital(entidad);
         return hospital;
+    }
+
+    @DELETE
+    @Path("{id: \\d+}")
+    public void deleteHospital(@PathParam("id") Long id) {
+        try {
+            logic.deleteHospital(id);
+        } catch (BusinessLogicException e) {
+            throw new WebApplicationException(e.getMessage(), 500);
+        }
     }
 
 }
