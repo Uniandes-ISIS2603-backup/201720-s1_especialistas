543cd510ed586cea860e88db25dba4d180d014a3
JOSE RENATO RESTOM VIERA <jr.restom10@sis.virtual.uniandes.edu.co>
2017-11-29 15:04:13 -0500

    relaciones de cita

--- especialistas-web/src/main/webapp/src/modules/ordenesMedicas/create/ordenesMedicas.create.ctrl.js
+++ especialistas-web/src/main/webapp/src/modules/ordenesMedicas/create/ordenesMedicas.create.ctrl.js
@@ -4,15 +4,27 @@
     mod.controller('ordenesMedicasCreateCtrl', ['$scope', '$http', 'ordenesMedicasContext', '$state', '$rootScope',
         function ($scope, $http, ordenesMedicasContext, $state, $rootScope) {
             $rootScope.edit = false;
-            $scope.citaParaOrdenId = $rootScope.glovalCitaId;
-           
+            $scope.citaParaOrdenId = $rootScope.glovalCitaId
+           $http.get("api/examenes").then(function (response) {
+                $scope.examenes = response.data;
+            });
 
-
+               
 
             $scope.createOrdenMedica = function () {
+                
+                a=[];
+                for(i=0;i<$scope.examenNuevo.length;i++)
+                {
+                    a.push($scope.$eval($scope.examenNuevo[i]))
+                    
+                }
+                $scope.examenesDeOrdenes=a;
+                
             $http.post(ordenesMedicasContext , {
                 descripcion: $scope.descripcion,
-                cita : $rootScope.glovalCita
+                cita : $rootScope.glovalCita,
+                examenes:a
                 
                 
                 
