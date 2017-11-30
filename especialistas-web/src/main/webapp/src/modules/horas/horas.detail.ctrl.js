6be5ef6b5b8691c63d6458a100569187fec03b38
Juan Sebastian Diaz <js.diaz@uniandes.edu.co>
2017-11-30 17:48:36 -0500

    resuelto detail horas

--- especialistas-web/src/main/webapp/src/modules/horas/horas.detail.ctrl.js
+++ especialistas-web/src/main/webapp/src/modules/horas/horas.detail.ctrl.js
@@ -1,22 +1,21 @@
 (function (ng) {
     var mod = ng.module("horaModule");
     mod.constant("horasContext", "api/horas");
-    mod.constant("citasContext", "api/citas");
     mod.constant("consultoriosContext", "api/consultorios");
-    mod.controller('horaDetailCtrl', ['$scope', '$state', '$rootScope', '$http', 'horasContext', 'citasContext', 'consultoriosContext',
-        function ($scope, $state, $rootScope, $http, horasContext, citasContext, consultoriosContext) {
+    mod.controller('horaDetailCtrl', ['$scope', '$state', '$http', 'horasContext',
+        function ($scope, $state, $http, horasContext) {
             
             if ($state.params.horaId !== undefined) {
                 $http.get(horasContext + '/' + $state.params.horaId).then(function (response) {
                     $scope.hora = response.data;
-                    $rootScope.hayCita = false;
+                    $scope.hayCita = false;
                     if ($scope.hora.cita !== undefined) {
-                        $rootScope.hayCita = true;
-                        $http.get(citasContext + '/' + $scope.hora.cita.id).then(function (response) {
+                        $scope.hayCita = true;
+                        $http.get('api/citas/' + $scope.hora.cita.id).then(function (response) {
                             $scope.cita = response.data;
                         });
                     }
-                    $http.get(consultoriosContext + '/' + $scope.hora.consultorio.id).then(function (response) {
+                    $http.get( 'api/consultorios/' + $scope.hora.consultorio.id).then(function (response) {
                         $scope.consultorio = response.data;
                     });
 
